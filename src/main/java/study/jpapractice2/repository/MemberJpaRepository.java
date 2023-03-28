19fef225952b2d6fc3be943a020a1ffc172a00b2
cruway <astralkaiser89@gmail.com>
2023-03-28 13:32:54 +0900

    jpa example code complete

--- src/main/java/study/jpapractice2/repository/MemberJpaRepository.java
+++ src/main/java/study/jpapractice2/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import study.jpapractice2.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 @RequiredArgsConstructor
 public class MemberJpaRepository {
@@ -14,6 +17,29 @@ public class MemberJpaRepository {
     public Member save(Member member) {
         em.persist(member);
         return member;
+    }
+
+    public void update(Member member) {
+
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
     }
 
     public Member find(Long id) {
